a88db6f123db2bd468c5cf43d905b6ceb52b183b
Rex Fung <rexfung@umich.edu>
2023-02-21 17:33:40 -0500

    Changed to plot full k_traj rather than just sampled points to better reflect time constraint

--- viz.py
+++ viz.py
@@ -3,12 +3,13 @@
 
 from matplotlib import pyplot as plt
 
-def plot2Dkspace(Nx, Ny, k_traj_adc):
+def plot2Dkspace(k_traj_adc, k_traj, t_excitation, t_refocusing, t_adc):
     plt.figure(1)
-    plt.plot(k_traj_adc[0,0:Nx*Ny],k_traj_adc[1,0:Nx*Ny],'k',label='trajectory')
+    plt.plot(k_traj[0,:],k_traj[1,:],'k',label='trajectory')
     plt.scatter(k_traj_adc[0,:],k_traj_adc[1,:],marker="x", label='acquired samples')
-    plt.scatter(k_traj_adc[0,0],k_traj_adc[1,0],marker="X", label='start')
-    plt.scatter(k_traj_adc[0,-1],k_traj_adc[1,-1],marker="X", label='end')
+    plt.scatter(k_traj_adc[0,0],k_traj_adc[1,0],marker="X", label='first sample')
+    plt.scatter(k_traj_adc[0,-1],k_traj_adc[1,-1],marker="X", label='last sample')
+    plt.axis('equal')
     plt.xlabel('$k_{x} (m^{-1})$')
     plt.ylabel('$k_{y} (m^{-1}$)')
     plt.legend(bbox_to_anchor=(1,1))

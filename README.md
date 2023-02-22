4107e876e930030faf8d7b97cacc0a54f7246ef1
Rex Fung <rexfung@umich.edu>
2023-02-22 18:05:46 -0500

    anagnorisis

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Feb 21, 2023
-Started a devlog to keep track of what I did.
+- Started a devlog to keep track of what I did.
+- Sequences owned: One line, vanilla GRE, vanilla EPI
+- Functions owned: viz.py
 
-Sequences owned: One line, vanilla GRE, vanilla EPI
-Functions owned: viz.py
+# Feb 22, 2023
+- increased resolution of plot2Dkspace()
+- wrote seq2toppe.m to convert .seq files to GE using PulseGEq
+- was gonna scan but ran into compatibility issues between PyPulseq and Pulseq
+- resolved issue by upgrading pypulseq module by pip installing latest version (1.4.0) downloaded from Github
--- viz.py
+++ viz.py
@@ -3,12 +3,15 @@
 
 from matplotlib import pyplot as plt
 
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 300
+
 def plot2Dkspace(k_traj_adc, k_traj, t_excitation, t_refocusing, t_adc):
     plt.figure(1)
-    plt.plot(k_traj[0,:],k_traj[1,:],'k',label='trajectory')
-    plt.scatter(k_traj_adc[0,:],k_traj_adc[1,:],marker="x", label='acquired samples')
-    plt.scatter(k_traj_adc[0,0],k_traj_adc[1,0],marker="X", label='first sample')
-    plt.scatter(k_traj_adc[0,-1],k_traj_adc[1,-1],marker="X", label='last sample')
+    plt.plot(k_traj[0,:],k_traj[1,:],'k',linewidth=1,label='trajectory',zorder=1)
+    plt.scatter(k_traj_adc[0,:],k_traj_adc[1,:],marker=".", label='acquired samples',zorder=2)
+    plt.scatter(k_traj_adc[0,0],k_traj_adc[1,0],marker="X", label='first sample',zorder=2)
+    plt.scatter(k_traj_adc[0,-1],k_traj_adc[1,-1],marker="X", label='last sample',zorder=2)
     plt.axis('equal')
     plt.xlabel('$k_{x} (m^{-1})$')
     plt.ylabel('$k_{y} (m^{-1}$)')
